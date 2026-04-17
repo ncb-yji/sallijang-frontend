f749a45a408e69e7f846f7caa43e5a8bf27d94b5
ncb-yji <clanic.otl@gmail.com>
2026-04-17 14:58:42 +0900

    mouseevent fixed

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef, useEffect, type MouseEvent } from 'react';
 import { formatCountdown } from '../utils/timeUtils';
 import type { Product } from '../types';
 
